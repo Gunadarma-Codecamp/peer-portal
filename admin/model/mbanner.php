95004dd3f6e96a6cbb531ec6b89a7c0445722da2
kartikaDS <dhitatracker@gmail.com>
2015-06-17 02:31:45 +0700

    Admin - Banner

--- admin/model/mbanner.php
+++ admin/model/mbanner.php
@@ -18,17 +18,10 @@ class mbanner extends Database {
 		if($data['action'] == 'insert'){
 			//pr($data);exit;
 			//yang atas nama variabel @ db.
-			$query = "INSERT INTO  
-			  
-						{$this->prefix}_banner (
-                            icon_image,file_icon,langid,
-                            title_bhs,brief_bhs,content_bhs,
-                            image,file,created_date,
-                            posted_date,author_id,n_stats)
-					VALUES
-						('".$data['icon']."','".$data['icon_url']."','".$data['langID']."'
-                        ,'".$data['title_bhs']."','".$data['brief_bhs']."','".$data['content_bhs']."'
-                        ,'".$data['image']."','".$data['image_url']."','".$date."'
+			$query = "INSERT INTO {$this->prefix}_banner 
+						(description,image,file,created_date,posted_date,author_id,n_stats)
+					  VALUES
+						('".$data['description']."','".$data['image']."','".$data['image_url']."','".$date."'
                         ,'".$data['postdate']."','".$data['authorid']."','".$data['n_stats']."')";
                         
                         //pr($query);exit;
@@ -37,14 +30,9 @@ class mbanner extends Database {
             if($data['categoryid']=='1' && $data['articletype']=='2') $date = $data['postdate'];
 			$query = "UPDATE {$this->prefix}_banner
 						SET 
-							title_bhs = '{$data['title_bhs']}',
-							brief_bhs = '{$data['brief_bhs']}',
-							content_bhs = '{$data['content_bhs']}',
-                            
+							description = '{$data['description']}',
 							image = '{$data['image']}',
 							file = '{$data['image_url']}',
-							icon_image = '{$data['icon']}',
-							file_icon = '{$data['icon_url']}',
 							posted_date = '".$date."',
                             expired_date = '{$data['expired_date']}',
 							author_id = '{$data['authorid']}',
@@ -88,23 +76,6 @@ class mbanner extends Database {
     
     		$result['username'] = $username['username'];
 		}
-		return $result;
-	}
-	
-	function get_banner_trash($categoryid=null)
-	{
-		$query = "SELECT * FROM {$this->prefix}_banner WHERE n_status = '2' AND categoryid = '{$categoryid}' ORDER BY created_date DESC";
-		
-		$result = $this->fetch($query,1);
-
-		foreach ($result as $key => $value) {
-			$query = "SELECT username FROM admin_member WHERE id={$value['authorid']} LIMIT 1";
-
-			$username = $this->fetch($query,0);
-
-			$result[$key]['username'] = $username['username'];
-		}
-		
 		return $result;
 	}
 	
