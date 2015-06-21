167efa39e7ebf64b7330b656174f8b78766c92b6
kartikaDS <dhitatracker@gmail.com>
2015-06-22 03:47:51 +0700

    Retrieve Data Digirepo - Workshop Material

--- app/controller/digirepo.php
+++ app/controller/digirepo.php
@@ -27,18 +27,20 @@ class digirepo extends Controller {
 		//pr($data);
 		if($data){
 			foreach ($data as $key => $value) {
+				$data[$key]['created_date'] = dateFormat($value['created_date'],'article');
 				$data[$key]['content'] = html_entity_decode($value['content'],ENT_QUOTES, 'UTF-8');
 			}
 			
 			$this->view->assign('data',$data);
 		}
 
+		$this->view->assign('title','Reference');
         return $this->loadView('digirepo/reference');
     }
     
-    function referencesDetail(){
+    function repositoryDetail(){
     	$id = $_GET['id'];
-    	$where = "category = '1' AND id = '".$id."' AND n_status = '1'";
+    	$where = "id = '".$id."' AND n_status = '1'";
 		$data = $this->models->getData(TRUE,'*','floraINA_digirepo',$where);
 		//pr($data);
 		if($data){
@@ -54,11 +56,40 @@ class digirepo extends Controller {
     }
     
     function workshop(){
+    	$where = "n_status = '1'";
+		$data = $this->models->getData(TRUE,'*','floraINA_digirepo_events',$where);
+		//pr($data);
+		if($data){
+			foreach ($data as $key => $value) {
+				$data[$key]['start_date'] = dateFormat($value['start_date'],'article');
+				$data[$key]['end_date'] = dateFormat($value['end_date'],'article');
+				$data[$key]['created_date'] = dateFormat($value['created_date'],'article');
+				$data[$key]['content'] = html_entity_decode($value['content'],ENT_QUOTES, 'UTF-8');
+			}
+			
+			$this->view->assign('data',$data);
+		}
+
         return $this->loadView('digirepo/workshop');
     }
     
     function workshopDetail(){
-        return $this->loadView('digirepo/workshop_detail');
+    	$id = $_GET['id'];
+    	$where = "category = '2' AND otherid = '".$id."' AND n_status = '1'";
+		$data = $this->models->getData(TRUE,'*','floraINA_digirepo',$where);
+		//pr($data);
+		if($data){
+			foreach ($data as $key => $value) {
+				$data[$key]['created_date'] = dateFormat($value['created_date'],'article');
+				$data[$key]['content'] = html_entity_decode($value['content'],ENT_QUOTES, 'UTF-8');
+			}
+			
+			$this->view->assign('data',$data);
+		}
+
+		$this->view->assign('title','Workshop Material');
+        return $this->loadView('digirepo/reference');
+        //return $this->loadView('digirepo/workshop_detail');
     }
 }
 
