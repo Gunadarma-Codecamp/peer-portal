a932789c91a3197507197c4c50edd6c7bbdb37a0
kartikaDS <dhitatracker@gmail.com>
2015-06-22 03:54:12 +0700

    Admin - Digirepo Filesize fixes

--- admin/model/mdigirepo.php
+++ admin/model/mdigirepo.php
@@ -2,55 +2,6 @@
 class mdigirepo extends Database {
 	
     var $prefix = "floraINA";
-    /**
-     * @todo insert a record to database
-     * 
-     * @param $table = table name
-     * @param $data = array data to insert
-     * @param $db2 = boolean using second database or not
-     * 
-     * @return lastid = id of inserted data
-     * @return status = boolean status of data
-     * 
-     * */
-	function insertData($table=false, $data=array(), $db2=false)
-	{
-        global $CONFIG, $basedomain;
-		if (!$table and empty($data)) return false;
-
-		$return = array();
-		$return['n_status'] = false;
-        
-        if(!empty($data['created_date'])) $data['created_date'] = date("Y-m-d H:i:s",strtotime($data['created_date']));
-        if(!empty($data['expired_date'])) $data['expired_date'] = date("Y-m-d H:i:s",strtotime($data['expired_date']));
-		
-		foreach ($data as $key=>$val){
-            if(!empty($val)){
-                
-                $sanitize = str_replace(array('\'', '"'), '', $val);
-                
-                $tmpfield[] = "`$key`";
-                $tmpvalue[] = "'{$sanitize}'";
-            }
-		}
-		
-		$field = implode (',',$tmpfield);
-		$value = implode (',',$tmpvalue);
-		
-		$sql = "INSERT INTO {$this->prefix}_{$table} ({$field}) VALUES ({$value})";
-        
-        if($db2){
-            $res = $this->query($sql,1);
-        }else{
-            $res = $this->query($sql);
-        }
-        
-		if ($res){
-			$return['lastid'] = $this->insert_id();
-			$return['n_status'] = true;
-		}
-		return $return;
-	}
     
     function repoInp($table,$data)
 	{
@@ -84,6 +35,12 @@ class mdigirepo extends Database {
                         //pr($query);exit;
 
 		} else {
+			if($data['filesize'] == ''){
+				$filesize = "";
+			}
+			else{
+				$filesize = "filesize = '".$data['filesize']."',";
+			}
 			$query = "UPDATE {$this->prefix}_{$table}
 						SET 
 							title = '{$data['title']}',
@@ -93,7 +50,7 @@ class mdigirepo extends Database {
 							authorid = '{$data['authorid']}',
 							files = '{$data['files']}',
                             filename = '{$data['filename']}',
-                            filesize = '{$data['filesize']}',
+                            ".$filesize."
                             realname = '{$data['realname']}',
                             
                             icon = '{$data['icon']}',
