a354fba65323267243d53abf897fed81e47d994d
kartikaDS <dhitatracker@gmail.com>
2015-07-03 03:11:18 +0700

    Add Expedition page

---
+++ app/controller/expedition.php
@@ -0,0 +1,38 @@
+<?php
+
+class expedition extends Controller {
+	
+	var $models = FALSE;
+	var $view;
+
+	
+	function __construct()
+	{
+		global $basedomain;
+		$this->loadmodule();
+		$this->view = $this->setSmarty();
+		$this->view->assign('basedomain',$basedomain);
+    }
+	
+	function loadmodule()
+	{
+        $this->models = $this->loadModel('contentHelper');
+	}
+	
+	function index(){
+		$where = "categoryid = '2' AND articleType = '0' AND n_status = '1'";
+		$data = $this->models->getData(TRUE,'*','floraINA_news_content',$where);
+		//pr($data);
+		if($data){
+			foreach ($data as $key => $value) {
+				$data[$key]['content'] = html_entity_decode($value['content'],ENT_QUOTES, 'UTF-8');
+			}
+			
+			$this->view->assign('data',$data);
+		}
+		
+    	return $this->loadView('expedition');
+    }
+}
+
+?>
