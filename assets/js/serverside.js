b02fe63c6552cea677a70f26dd0abf1639e723e8
Ovan Pulu <ovan89@gmail.com>
2015-07-25 18:20:04 +0700

    cross domain

--- assets/js/serverside.js
+++ assets/js/serverside.js
@@ -20,7 +20,16 @@
 
                 "bProcessing": true,
                 "bServerSide": true,
-                "sAjaxSource": appdomain+"services/"+urlApi
+                "sAjaxSource": appdomain+"services/"+urlApi,
+                "fnServerData": function ( sSource, aoData, fnCallback, oSettings ) {
+			      oSettings.jqXHR = $.ajax( {
+			        "dataType": 'json',
+			        "type": "GET",
+			        "url": sSource,
+			        "data": aoData,
+			        "success": fnCallback
+			      } );
+			    }
 
             });
             
